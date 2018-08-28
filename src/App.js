75ce5cd67f07a65b2649e08f8e6d1c52e314c491
amirplanet <amir.waisman@planet.com>
2018-08-28 16:13:36 +0200

    App skeleton

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+import store from './store';
+import { Provider } from 'react-redux';
+import Board from './components/Board.js';
+
+class App extends Component {
+	render() {
+		return (
+			<Provider store={store}>
+				<div className="App">
+					<header className="App-header">
+						<h1 className="App-title">X / O</h1>
+					</header>
+					<Board />
+				</div>
+			</Provider>
+		);
+	}
+}
+
+export default App;
